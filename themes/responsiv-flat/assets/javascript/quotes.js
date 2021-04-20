c4c0b6e974a94b7c7fc1a0e5b57387aaa3018a91
Joe Davies <joesorton@gmail.com>
2021-04-20 11:45:37 +0100

    Load a quote from api

--- themes/responsiv-flat/assets/javascript/quotes.js
+++ themes/responsiv-flat/assets/javascript/quotes.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
 	$.ajax({url: "http://api.whoajoe.com/quotes.json", success: function(result){
-		console.log(result);
+		
+		var quoteElement = $("#quote");
+
+		var randomIndex = Math.floor(Math.random()*result.length);
+
+		var quoteText = '"' + result[randomIndex]["quote"] + '"';
+
+		quoteElement.hide();
+
+		quoteElement.text(quoteText);
+
+		quoteElement.fadeIn(3000);
+
 	}});
 });
